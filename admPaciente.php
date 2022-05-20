e518f097a12d1836cc759a187a8d87e00f14a8d9
Mattias <duartemattias3@gmail.com>
2022-05-19 20:45:37 -0400

    Se corrigio el row

--- admPaciente.php
+++ admPaciente.php
@@ -15,18 +15,18 @@
 <body>
     <section class="form section container pt-5">
         <div class="row flex">
-            <div class="col-6">
+            <div class="col-6 text-center">
                 <h2>Administra tus Pacientes!</h2>
                 <form action="ctrlPaciente.php" method="POST" enctype ="multipart/form-data">
                     
-                    <div class="mb-3">
+                    <div class="mb-3 text-start">
                         <label for="nomPac" class="form-label">Nombre</label>
                         <input type="text" class="form-control" name="nomPac" id="nomPac" aria-describedby="helpId"
                             placeholder="Ingresa el nombre del pacietne" require>
                     </div>
 
 
-                    <div class="mb-3">
+                    <div class="mb-3 text-start">
                         <label for="imgPac" class="form-label">Agrega la foto del cliente</label>
                         <input type="file" class="form-control" name="imgPac" id="imgPac" placeholder="Imagen"
                             aria-describedby="fileHelpId" require>
@@ -35,10 +35,11 @@
                     <div class="modal-footer btn-group">
                         <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
                         <input type="submit" value="Guardar" class="btn btn-primary" name="btn" id="btn">
+                    </div>
                 </form>
             </div>
 
-            <div class="col-6">
+            <div class="col-6 text-center">
                 <h2>Listado Pacientes</h2>
             </div>
         </div>
